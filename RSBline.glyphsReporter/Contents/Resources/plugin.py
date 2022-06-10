94dc8fbff98eb37c76a67c5216d71cab370c9910
Rainer Scheichelbauer <res@glyphsapp.com>
2022-06-10 12:35:55 -0500

    v1.02: streamlining, localisations, do not draw triangle (confusing)

--- RSBline.glyphsReporter/Contents/Resources/plugin.py
+++ RSBline.glyphsReporter/Contents/Resources/plugin.py
@@ -19,41 +19,51 @@ from GlyphsApp.plugins import *
 from AppKit import NSBezierPath, NSColor
 from math import tan, radians
 
-
 class showRSB(ReporterPlugin):
-
 
 	@objc.python_method
 	def settings(self):
 		self.menuName = Glyphs.localize({
-			'en': 'Show RSB',
-			'de': 'Mein Plugin',
-			'fr': 'Ma extension',
-			'es': 'Mi plugin',
-			'pt': 'Meu plug-in',
+			'en': 'RSB',
+			'de': 'Nachbreite',
+			'fr': 'approche droite',
+			'es': 'lado derecho',
 			})
-
+	
 	@objc.python_method
-	def drawLine(self, x1, y1, x2, y2, strokeWidth,):
+	def conditionsAreMetForDrawing(self):
+		"""
+		Only activate if text or pan (hand) tool are active.
+		"""
+		currentController = self.controller.view().window().windowController()
+		if currentController:
+			tool = currentController.toolDrawDelegate()
+			textToolIsActive = tool.isKindOfClass_( NSClassFromString("GlyphsToolText") )
+			if textToolIsActive: 
+				return True
+		return False
+	
+	@objc.python_method
+	def drawLine(self, x1, y1, x2, y2, strokeWidth):
 		path = NSBezierPath.bezierPath()
-		path.moveToPoint_((x1, y1))
-		path.lineToPoint_((x2, y2))
+		path.moveToPoint_(NSPoint(x1, y1))
+		path.lineToPoint_(NSPoint(x2, y2))
 		path.setLineWidth_(strokeWidth)
+		NSColor.separatorColor().colorWithAlphaComponent_(0.4).set()
 		path.stroke()
 
 	@objc.python_method
-	def drawTriangle(self, x1, y1, x2, y2, x3, y3, strokeWidth, color):
+	def drawTriangle(self, x1, y1, x2, y2, x3, y3):
 		path = NSBezierPath.bezierPath()
-		path.moveToPoint_((x1, y1))
-		path.lineToPoint_((x2, y2))
-		path.lineToPoint_((x3, y3))
+		path.moveToPoint_(NSPoint(x1, y1))
+		path.lineToPoint_(NSPoint(x2, y2))
+		path.lineToPoint_(NSPoint(x3, y3))
 		path.closePath()
-		path.setLineWidth_(strokeWidth)
-		color.set()
+		NSColor.controlAccentColor().colorWithAlphaComponent_(0.4).set()
 		path.fill()
 	
 	@objc.python_method
-	def italo(self, yPos, angle, xHeight):
+	def italicShift(self, yPos, angle, xHeight):
 		'''
 		ITALIC OFFSET. TAKEN FROM MARK FROMBERG'S SMART PLUMBLINES
 		'''
@@ -63,40 +73,31 @@ class showRSB(ReporterPlugin):
 
 	@objc.python_method
 	def drawRSB(self, layer):
-
-		angle = layer.master.italicAngle
+		angle = layer.italicAngle
 		xHeight = layer.master.xHeight
-		scale = Glyphs.font.currentTab.scale
 		x1, y1 = layer.width, layer.ascender
 		x2, y2 = x1, layer.descender
-		strokeWidth = 1 * scale
-		triColor = 	NSColor.colorWithRed_green_blue_alpha_(0.4, 0.7, 0.9, 0.4)
-		print(self.italo(x1, angle, xHeight))
-		self.drawLine(x1 + self.italo(y1, angle, xHeight), y1 , x2 + + self.italo(y2, angle, xHeight), y2, strokeWidth)
 		
-		self.drawTriangle((x1-25) + self.italo(y1, angle, xHeight), y1, x1 + self.italo(y1, angle, xHeight), (y1-25), (x1+25)+ self.italo(y1, angle, xHeight), y1, strokeWidth, triColor)
-		# self.drawTriangle((x1-25), y2, x1, (y2+25), (x2+25), y2, strokeWidth, triColor)
-
-	# @objc.python_method
-	# def foreground(self, layer):
-
-	# 	scale = Glyphs.font.currentTab.scale
-	# 	x1, y1 = layer.width, layer.ascender
-	# 	x2, y2 = x1, layer.descender
-	# 	strokeWidth = 1 * scale
-	# 	triColor = 	NSColor.colorWithRed_green_blue_alpha_(0.4, 0.7, 0.9, 0.4)
-	# 	angle = layer.master.italicAngle
+		# draw line:
+		strokeWidth = 1.0 * self.getScale() ** -0.9
+		self.drawLine( 
+			x1 + self.italicShift(y1, angle, xHeight), y1 , 
+			x2 + self.italicShift(y2, angle, xHeight), y2, 
+			strokeWidth,
+			)
 		
-	# 	self.drawLine(x1, self.italo(y1, angle), x2, self.italo(y2, angle), strokeWidth)
-		
-	# 	self.drawTriangle((x1-25), self.italo(y1, angle), x1, self.italo((y1-25), angle), (x1+25), self.italo(y1, angle), strokeWidth, triColor)
-	# 	self.drawTriangle((x1-25), y2, x1, (y2+25), (x2+25), y2, strokeWidth, triColor)
+		# triangle:
+		# triangleSize = 10.0 / self.getScale() # ** -0.9
+		# self.drawTriangle(
+		# 	x1-triangleSize + self.italicShift(y1, angle, xHeight), y1,
+		# 	x1 + self.italicShift(y1-triangleSize, angle, xHeight), (y1-triangleSize),
+		# 	x1+triangleSize + self.italicShift(y1, angle, xHeight), y1,
+		# 	)
 	
 	@objc.python_method
 	def background(self, layer):
-		
-		self.drawRSB(layer)
-
+		if self.conditionsAreMetForDrawing():
+			self.drawRSB(layer)
 
 	@objc.python_method
 	def __file__(self):
